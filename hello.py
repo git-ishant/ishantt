81ab8fda7ca6f76dd7e5ff1c01b8e35aea5a570d
git-ishant <8720127@gecnilokheri.ac.in>
2025-05-19 00:06:09 +0530

    file2

--- hello.py
+++ hello.py
@@ -4,9 +4,10 @@
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 num3 = float(input("Enter second number: "))
+num4 = float(input("Enter second number: "))
 
 # Calculate the sum
-sum = num1 + num2 + num3
+sum = num1 + num2 + num3 + num4
 
 # Display the result
 print("The sum is:", sum)
