70ee9fcef51976c542197e9761f16c490479dfe4
git-ishant <8720127@gecnilokheri.ac.in>
2025-05-18 23:59:57 +0530

    file1

---
+++ hello.py
@@ -0,0 +1,11 @@
+# Simple sum program in Python
+
+# Get input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calculate the sum
+sum = num1 + num2
+
+# Display the result
+print("The sum is:", sum)
